8af1109a0ba4271ba481c08bd3f545eebb68f83f
Emerald Alphante Reirezqi <emeraldalphanter@gmail.com>
2025-03-14 05:33:01 +0700

    check whether a string is a palindrome

---
+++ Tugas2/cekStringPalindrom.js
@@ -0,0 +1,8 @@
+function isPalindrome(str) {
+    const cleanedStr = str1.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    return cleanedStr === cleanedStr.split("").reverse().join("");
+}
+
+// Input dan Output
+const str = prompt("Masukkan string: ");
+console.log(`Apakah "${str}" palindrom? ${isPalindrome(str)}`);
