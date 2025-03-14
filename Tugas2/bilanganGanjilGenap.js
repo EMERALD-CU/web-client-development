9b22d61064ee74eacd87cbc5fab14979a310c8cc
Emerald Alphante Reirezqi <emeraldalphanter@gmail.com>
2025-03-14 14:50:40 +0700

    change the code to match output expectations

--- Tugas2/bilanganGanjilGenap.js
+++ Tugas2/bilanganGanjilGenap.js
@@ -1,7 +1,7 @@
 function isEven(n) {
-    return n % 2 === 0;
+    return n % 2 === 1;
 }
 
 // Input dan Output
 const number = parseInt(prompt("Masukkan bilangan bulat: "));
-console.log(`Apakah ${number} genap? ${isEven(number)}`);
+console.log(`Apakah ${number} ganjil? ${isEven(number)}`);
